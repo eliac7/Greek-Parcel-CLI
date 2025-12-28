19079400294e019eebf498f6719ad4137eda6bc9
eliac7 <elias.thal7@gmail.com>
2025-12-28 23:19:43 +0200

    feat(acs): implement method to fetch encrypted key from ACS website and update the DEFAULT_USER_AGENT to a modern browser

--- src/core/constants.py
+++ src/core/constants.py
@@ -2,7 +2,7 @@
 DEFAULT_TIMEOUT = 10
 DEFAULT_USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "(KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
 )
 
 # Courier Names
